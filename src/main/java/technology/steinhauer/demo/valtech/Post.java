df4761d7669b3733c454560f6620e651697c69ff
Holger Steinhauer <holger.steinhauer@prodyna.com>
2014-06-08 18:24:40 +0200

    Added Post representation and the TimelineService plus corresponding test class.

---
+++ src/main/java/technology/steinhauer/demo/valtech/Post.java
@@ -0,0 +1,53 @@
+package technology.steinhauer.demo.valtech;
+
+import java.util.Date;
+
+/**
+ * Represents one post, made by a specific user.
+ * As per the current requirements, a post is immutable because there is no post editing required by the "customer".
+ */
+public class Post {
+    private final String message;
+    private final String username;
+    private final Date date;
+
+    public Post(String username, String message, Date date) {
+        this.username = username;
+        this.message = message;
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Post post = (Post) o;
+
+        if (!date.equals(post.date)) return false;
+        if (!message.equals(post.message)) return false;
+        if (!username.equals(post.username)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = message.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + date.hashCode();
+        return result;
+    }
+}
